7016908b006b7c5ed27238147eeda6b025c2e3d6
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-02 14:51:26 +0200

    added footer-type and readme-instructions

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # DEVELOPER-INSTALLATION
+
 - Clone this repo into your wordplate themes folder.
 - cd back to your wordplate-folder.
 - Then run valet: "Valet link" the first time, then "valet open".
-- You should now be able to activate this theme in  /wp-admin.
+- You should now be able to activate this theme in /wp-admin.
+-
+
+Plugins:
+install Advanced Custom Fields.
+install ACF to REST API.
+
 # END OF INSTALLATION
---
+++ post-types/footer.php
@@ -0,0 +1,25 @@
+<?php
+
+declare (strict_types = 1);
+
+add_action('init', function () {
+    register_post_type('footers', [
+        'has_archive' => true,
+        'labels' => [
+            'add_new_item' => __('Add New Footer'),
+            'edit_item' => __('Edit Footer'),
+            'name' => __('Footer'),
+            'search_items' => __('Search Footers'),
+            'singular_name' => __('Footer'),
+        ],
+        'supports' => [
+            'title',
+            'editor',
+            'thumbnail',
+        ],
+        'menu_icon' => 'dashicons-admin-links',
+        'menu_position' => 20,
+        'public' => true,
+        'show_in_rest' => true
+    ]);
+});
