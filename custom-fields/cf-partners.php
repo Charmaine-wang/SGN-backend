1c01ad82d8b94ff87319d8c7707903de128b8b8e
Laureta Dzika <xhika_l@hotmail.com>
2019-04-17 08:54:23 +0200

    updated partners cf
    upload logo is now available

--- custom-fields/cf-partners.php
+++ custom-fields/cf-partners.php
@@ -2,42 +2,88 @@
 
 declare (strict_types = 1);
 
-
 // Adds custom field for Partners
 if( function_exists('acf_add_local_field_group') ):
 
-    acf_add_local_field_group([
-        'key' => 'group_5ca319457bdf2',
-        'title' => 'Add Partner',
-        'fields' => [
-            [
-                'key' => 'field_5ca3198201d40',
-                'label' => 'Add Partner',
-                'name' => 'link',
-                'type' => 'url',
-                'instructions' => 'Here you can insert a url to your partners and a feautered image.',
-                'required' => 0,
-                'conditional_logic' => 0,
-                'default_value' => '',
-                'placeholder' => 'https://website.com',
-            ],
-        ],
-        'location' => [
-            [
-                [
-                    'param' => 'post_type',
-                    'operator' => '==',
-                    'value' => 'partners',
-                ],
-            ],
-        ],
-        'menu_order' => 0,
-        'position' => 'normal',
-        'style' => 'default',
-        'label_placement' => 'top',
-        'instruction_placement' => 'label',
-        'hide_on_screen' => '',
-        'active' => true,
-        'description' => '',
-    ]);
+acf_add_local_field_group(array(
+    'key' => 'group_5cb6cbe5d7700',
+    'title' => 'Partners',
+    'fields' => array(
+        array(
+            'key' => 'field_5cb6cbee9be5c',
+            'label' => 'Partners Information',
+            'name' => 'partners_information',
+            'type' => 'group',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'layout' => 'block',
+            'sub_fields' => array(
+                array(
+                    'key' => 'field_5cb6cbfc9be5d',
+                    'label' => 'Link',
+                    'name' => 'link',
+                    'type' => 'url',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => 'https://www.website.com',
+                ),
+                array(
+                    'key' => 'field_5cb6cc1b9be5e',
+                    'label' => 'Logo',
+                    'name' => 'logo',
+                    'type' => 'image',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'return_format' => 'array',
+                    'preview_size' => 'thumbnail',
+                    'library' => 'all',
+                    'min_width' => '',
+                    'min_height' => '',
+                    'min_size' => '',
+                    'max_width' => '',
+                    'max_height' => '',
+                    'max_size' => '',
+                    'mime_types' => '',
+                ),
+            ),
+        ),
+    ),
+    'location' => array(
+        array(
+            array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'partners',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+));
+
 endif;
